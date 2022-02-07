1d7c7a01f962a2cf4f0e39a97bfde0e9675e3006
MikhailEvt <55846652+MikhailEvt@users.noreply.github.com>
2022-02-07 20:46:09 +0300

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,2 +1,11 @@
+#Get-ChildItem -Path ./ -Filter  "*-7.15.*.*" -Recurse |
+#Rename-Item -NewName {$_.Name -Replace "-7\.15\.[0-9]*\.*","-7.15.1235."}
+
+
+$path = Get-ChildItem -Path ./ -Filter  "PVS-*-sdh.txt"
+$path -match  "-7\.15\.(\d{4})\."
+$str = $matches[1]
+
+
 Get-ChildItem -Path ./ -Filter  "*-7.15.*.*" -Recurse |
-Rename-Item -NewName {$_.Name -Replace "-7\.15\.[0-9]*\.*","-7.15.1235."}
+Rename-Item -NewName {$_.Name -Replace "-7\.15\.[0-9]*\.*","-7.15.$($str)."}
